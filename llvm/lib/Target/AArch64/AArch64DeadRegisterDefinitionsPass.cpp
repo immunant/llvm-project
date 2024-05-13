351fde8821c35396b79ad309320962d1d517ac8a
Ayrton Munoz <adm@immunant.com>
2024-05-13 15:57:08 -0400

    Add MTE tagging for STR*/STP*/LDR*/LDP*
    Note that function prologues are not instrumented because it seems this pass
    doesn't run over those basic blocks.

--- llvm/lib/Target/AArch64/AArch64DeadRegisterDefinitionsPass.cpp
+++ llvm/lib/Target/AArch64/AArch64DeadRegisterDefinitionsPass.cpp
@@ -117,34 +117,202 @@ void AArch64DeadRegisterDefinitions::processMachineBasicBlock(
     MachineBasicBlock &MBB) {
   const MachineFunction &MF = *MBB.getParent();
   for (MachineBasicBlock::iterator II = MBB.begin(), E = MBB.end(); II != E; ++II) {
-      MachineInstr &MI = *II;
-  //for (MachineInstr &MI : MBB) {
-    if (
-        (MI.getOpcode() == AArch64::STRWui)) {
-        //(MI.getOpcode() == AArch64::STRWroW) ||
-        //(MI.getOpcode() == AArch64::STRWroX) ||
-        //(MI.getOpcode() == AArch64::STRWpre) ||
-        //(MI.getOpcode() == AArch64::STRWpost)) {
-
-      //MBB.setInstr(MI);
-      //auto it = MBB.getInsertPt()++;
-      //auto it = MI.getIterator();
+    MachineInstr &MI = *II;
+    if (MI.mayLoadOrStore()) {
+      bool tag = true;
+      unsigned operand_number = 0;
+      /*
+       * TODO: check if we need to do anything for LDRDl, LDRQl, LDRSWl, LDRSl,
+       * LDRWl and LDRXl. They load literals and have no GPR64sp operands but
+       * are marked as may load
+       */
+      switch (MI.getOpcode()) {
+        case AArch64::LDRBBroW:
+        case AArch64::LDRBBroX:
+        case AArch64::LDRBBui:
+        case AArch64::LDRBroW:
+        case AArch64::LDRBroX:
+        case AArch64::LDRBui:
+        case AArch64::LDRDroW:
+        case AArch64::LDRDroX:
+        case AArch64::LDRDui:
+        case AArch64::LDRHHroW:
+        case AArch64::LDRHHroX:
+        case AArch64::LDRHHui:
+        case AArch64::LDRHroW:
+        case AArch64::LDRHroX:
+        case AArch64::LDRHui:
+        case AArch64::LDRQroW:
+        case AArch64::LDRQroX:
+        case AArch64::LDRQui:
+        case AArch64::LDRSBWroW:
+        case AArch64::LDRSBWroX:
+        case AArch64::LDRSBWui:
+        case AArch64::LDRSBXroW:
+        case AArch64::LDRSBXroX:
+        case AArch64::LDRSBXui:
+        case AArch64::LDRSHWroW:
+        case AArch64::LDRSHWroX:
+        case AArch64::LDRSHWui:
+        case AArch64::LDRSHXroW:
+        case AArch64::LDRSHXroX:
+        case AArch64::LDRSHXui:
+        case AArch64::LDRSWroW:
+        case AArch64::LDRSWroX:
+        case AArch64::LDRSWui:
+        case AArch64::LDRSroW:
+        case AArch64::LDRSroX:
+        case AArch64::LDRSui:
+        case AArch64::LDRWroW:
+        case AArch64::LDRWroX:
+        case AArch64::LDRWui:
+        case AArch64::LDRXroW:
+        case AArch64::LDRXroX:
+        case AArch64::LDRXui:
+        case AArch64::LDR_PXI:
+        case AArch64::LDR_TX:
+        case AArch64::LDR_ZXI:
+        case AArch64::LDRAAindexed:
+        case AArch64::LDRABindexed:
+        case AArch64::STRBBroW:
+        case AArch64::STRBBroX:
+        case AArch64::STRBBui:
+        case AArch64::STRBroW:
+        case AArch64::STRBroX:
+        case AArch64::STRBui:
+        case AArch64::STRDroW:
+        case AArch64::STRDroX:
+        case AArch64::STRDui:
+        case AArch64::STRHHroW:
+        case AArch64::STRHHroX:
+        case AArch64::STRHHui:
+        case AArch64::STRHroW:
+        case AArch64::STRHroX:
+        case AArch64::STRHui:
+        case AArch64::STRQroW:
+        case AArch64::STRQroX:
+        case AArch64::STRQui:
+        case AArch64::STRSroW:
+        case AArch64::STRSroX:
+        case AArch64::STRSui:
+        case AArch64::STRWroW:
+        case AArch64::STRWroX:
+        case AArch64::STRWui:
+        case AArch64::STRXroW:
+        case AArch64::STRXroX:
+        case AArch64::STRXui:
+        case AArch64::STR_PXI:
+        case AArch64::STR_TX:
+        case AArch64::STR_ZXI:
+            operand_number = 1;
+            break;
+        case AArch64::LDPDi:
+        case AArch64::LDPQi:
+        case AArch64::LDPSWi:
+        case AArch64::LDPSi:
+        case AArch64::LDPWi:
+        case AArch64::LDPXi:
+        case AArch64::LDRAAwriteback:
+        case AArch64::LDRABwriteback:
+        case AArch64::LDRBBpost:
+        case AArch64::LDRBBpre:
+        case AArch64::LDRBpost:
+        case AArch64::LDRBpre:
+        case AArch64::LDRDpost:
+        case AArch64::LDRDpre:
+        case AArch64::LDRHHpost:
+        case AArch64::LDRHHpre:
+        case AArch64::LDRHpost:
+        case AArch64::LDRHpre:
+        case AArch64::LDRQpost:
+        case AArch64::LDRQpre:
+        case AArch64::LDRSBWpost:
+        case AArch64::LDRSBWpre:
+        case AArch64::LDRSBXpost:
+        case AArch64::LDRSBXpre:
+        case AArch64::LDRSHWpost:
+        case AArch64::LDRSHWpre:
+        case AArch64::LDRSHXpost:
+        case AArch64::LDRSHXpre:
+        case AArch64::LDRSWpost:
+        case AArch64::LDRSWpre:
+        case AArch64::LDRSpost:
+        case AArch64::LDRSpre:
+        case AArch64::LDRWpost:
+        case AArch64::LDRWpre:
+        case AArch64::LDRXpost:
+        case AArch64::LDRXpre:
+        case AArch64::STPDi:
+        case AArch64::STPQi:
+        case AArch64::STPSi:
+        case AArch64::STPWi:
+        case AArch64::STPXi:
+        case AArch64::STRBBpost:
+        case AArch64::STRBBpre:
+        case AArch64::STRBpost:
+        case AArch64::STRBpre:
+        case AArch64::STRDpost:
+        case AArch64::STRDpre:
+        case AArch64::STRHHpost:
+        case AArch64::STRHHpre:
+        case AArch64::STRHpost:
+        case AArch64::STRHpre:
+        case AArch64::STRQpost:
+        case AArch64::STRQpre:
+        case AArch64::STRSpost:
+        case AArch64::STRSpre:
+        case AArch64::STRWpost:
+        case AArch64::STRWpre:
+        case AArch64::STRXpost:
+        case AArch64::STRXpre:
+            operand_number = 2;
+            break;
+        case AArch64::LDPDpost:
+        case AArch64::LDPDpre:
+        case AArch64::LDPQpost:
+        case AArch64::LDPQpre:
+        case AArch64::LDPSWpost:
+        case AArch64::LDPSWpre:
+        case AArch64::LDPSpost:
+        case AArch64::LDPSpre:
+        case AArch64::LDPWpost:
+        case AArch64::LDPWpre:
+        case AArch64::LDPXpost:
+        case AArch64::LDPXpre:
+        case AArch64::LDR_ZA:
+        case AArch64::STPDpost:
+        case AArch64::STPDpre:
+        case AArch64::STPQpost:
+        case AArch64::STPQpre:
+        case AArch64::STPSpost:
+        case AArch64::STPSpre:
+        case AArch64::STPWpost:
+        case AArch64::STPWpre:
+        case AArch64::STPXpost:
+        case AArch64::STPXpre:
+        case AArch64::STR_ZA:
+            operand_number = 3;
+            break;
+        default:
+            tag = false;
+            break;
+      }
+      assert(tag);
       const MCInstrDesc &EXTRII = TII->get(AArch64::EXTRWrri);
 
-      auto op = MI.getOperand(1);
-      if (op.isReg()) {
-          auto reg = op.getReg();
-          MachineInstrBuilder MIB2 = BuildMI(MBB, MI, MI.getDebugLoc(), EXTRII);
-          MIB2.addReg(reg).addReg(reg).addReg(AArch64::X18).addImm(56);
+      auto op = MI.getOperand(operand_number);
+      LLVM_DEBUG(dbgs() << "opcode num is " << MI.getOpcode() << " checking operand " << operand_number << "\n");
+      assert(op.isReg());
+      auto reg = op.getReg();
+      MachineInstrBuilder MIB2 = BuildMI(MBB, MI, MI.getDebugLoc(), EXTRII);
+      MIB2.addReg(reg).addReg(reg).addReg(AArch64::X18).addImm(56);
 
-          //it++;
-          MachineInstrBuilder MIB3 = BuildMI(MBB, MI, MI.getDebugLoc(), EXTRII);
-          MIB3.addReg(reg).addReg(reg).addReg(reg).addImm(4);
-          II = MIB3;
-          II++;
-          Changed = true;
-          continue;
-      }
+      MachineInstrBuilder MIB3 = BuildMI(MBB, MI, MI.getDebugLoc(), EXTRII);
+      MIB3.addReg(reg).addReg(reg).addReg(reg).addImm(4);
+      II = MIB3;
+      II++;
+      Changed = true;
+      continue;
     }
     if (usesFrameIndex(MI)) {
       // We need to skip this instruction because while it appears to have a

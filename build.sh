ed96523153fa2b0a34226dff153eb75f48cac68c
Ayrton Munoz <adm@immunant.com>
2024-05-16 15:48:30 -0400

    Set default target triple in build script
    Clang still can't link because the Scrt1.o the clang driver picks up is the x86
    one. For example:
    /usr/bin/aarch64-linux-gnu-ld: /usr/lib64/gcc/aarch64-linux-gnu/13.2.0/../../../../lib64/Scrt1.o: Relocations in generic ELF (EM: 62)

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
+# May need to set -DLLVM_TARGETS_TO_BUILD="AArch64;X86" for CMake to build some
+# of the test programs it compiles at config-time. This may also require setting
+# the default target triple to x86_64-linux-gnu
 mkdir -p build
 cd build
-cmake -GNinja -DLLVM_ENABLE_PROJECTS=clang \
+cmake -GNinja -DLLVM_ENABLE_PROJECTS="clang" \
     -DLLVM_TARGETS_TO_BUILD="AArch64" \
     -DCMAKE_BUILD_TYPE=Debug \
+    -DLLVM_DEFAULT_TARGET_TRIPLE="aarch64-linux-gnu" \
     ../llvm
 ninja clang

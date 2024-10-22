b510037a11c075be6eed8e007f2361bec7a8f07f
Frances Wingerter <fw@immunant.com>
2024-10-22 19:59:49 -0400

    futz about some more

--- cross-build-rtlibs.sh
+++ cross-build-rtlibs.sh
@@ -10,7 +10,7 @@ crt_candidates="$(ls -d /usr/lib/gcc/aarch64-linux-gnu/*)
 $(ls -d /usr/lib/gcc-cross/aarch64-linux-gnu/*)"
 crt_dir=$(echo $crt_candidates | sort -V | tail -n1)
 
-cross_flags="-B$crt_dir --sysroot=/usr/aarch64-linux-gnu --gcc-install-dir=$crt_dir -isystem /usr/aarch64-linux-gnu/include --rtlib=compiler-rt -march=armv8+memtag -ffixed-x18"
+cross_flags="-B$crt_dir --sysroot=/usr/aarch64-linux-gnu --gcc-install-dir=$crt_dir -isystem /usr/aarch64-linux-gnu/include -march=armv8+memtag -ffixed-x18"
 #--sysroot=/usr/aarch64-linux-gnu/ --gcc-install-dir=/usr/lib/gcc/aarch64-linux-gnu/14.1.0
 export LDFLAGS="-L/usr/aarch64-linux-gnu/lib"
 cmake -GNinja -DLLVM_TARGETS_TO_BUILD="AArch64" -DLLVM_DEFAULT_TARGET_TRIPLE="aarch64-linux-gnu" \
@@ -18,8 +18,8 @@ cmake -GNinja -DLLVM_TARGETS_TO_BUILD="AArch64" -DLLVM_DEFAULT_TARGET_TRIPLE="aa
     -DCMAKE_C_FLAGS="$cross_flags --target=aarch64-linux-gnu" -DCMAKE_CXX_FLAGS="$cross_flags --target=aarch64-linux-gnu" \
     -DCMAKE_C_COMPILER="$(pwd)/../build/bin/clang" -DCMAKE_CXX_COMPILER="$(pwd)/../build/bin/clang" \
     -DCMAKE_BUILD_WITH_INSTALL_RPATH=true \
-    -DCMAKE_EXE_LINKER_FLAGS='--rtlib=compiler-rt' \
     -DCOMPILER_RT_BUILD_BUILTINS=ON \
+    -DCOMPILER_RT_BUILD_SANITIZERS=OFF -DCOMPILER_RT_BUILD_MEMPROF=OFF -DCOMPILER_RT_BUILD_ORC=OFF -DCOMPILER_RT_BUILD_XRAY=OFF -DCOMPILER_RT_BUILD_LIBFUZZER=OFF \
     -DLIBCXX_USE_COMPILER_RT=YES \
     -DLLVM_ENABLE_RUNTIMES='libcxx;libcxxabi;compiler-rt;libunwind' \
     ../runtimes

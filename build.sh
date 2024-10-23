7e4d587bd0b05f7a6c43b9c3d06f866632fc64cd
Frances Wingerter <fw@immunant.com>
2024-10-23 15:12:30 -0400

    compile builtins with built compiler, passing -ffixed-x18

--- build.sh
+++ build.sh
@@ -3,9 +3,13 @@
 # the default target triple to x86_64-linux-gnu
 mkdir -p build
 cd build
+export CFLAGS="-ffixed-x18"
+export CXXFLAGS="-ffixed-x18"
 cmake -GNinja -DLLVM_ENABLE_PROJECTS="clang" \
     -DLLVM_TARGETS_TO_BUILD="AArch64" \
     -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+    -DCOMPILER_RT_BUILD_BUILTINS=ON \
+    -DLLVM_ENABLE_RUNTIMES="compiler-rt" \
     -DLLVM_DEFAULT_TARGET_TRIPLE="aarch64-linux-gnu" \
     ../llvm
-ninja clang
+ninja clang builtins

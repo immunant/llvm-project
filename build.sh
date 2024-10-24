07990b55da80fb8010c364802f74007bd06e2f0b
Frances Wingerter <fw@immunant.com>
2024-10-24 09:12:46 -0400

    ci: try to save some disk space

--- build.sh
+++ build.sh
@@ -12,6 +12,14 @@ cmake -GNinja -DLLVM_ENABLE_PROJECTS="clang" \
     -DLLVM_ENABLE_RUNTIMES="compiler-rt" \
     -DLLVM_DEFAULT_TARGET_TRIPLE="aarch64-linux-gnu" \
     ../llvm
-ninja clang builtins
+ninja clang
+# free up disk space by stubbing out files that aren't needed for the next targets
+rm -rf _CPack_Packages
+for file in lib/libclangStaticAnalyzerCheckers.a lib/libclangCodeGen.a lib/libclangSema.a lib/libclangAST.a lib/libclangStaticAnalyzerCore.a lib/CodeGen/**/CMakeFiles/*.dir/*.o lib/Transforms/**/CMakeFiles/*.dir/*.o; do
+	mv $file $file.bak
+	touch $file --reference=$file.bak
+	rm $file.bak
+done
+ninja builtins
 # copy to expected target triple
 cp -arv lib/clang/19/lib/aarch64-linux-gnu lib/clang/19/lib/aarch64-unknown-linux-gnu
